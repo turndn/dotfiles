2d128805f6060caa45d9694c4557c240e6adef57
Kenta Ishiguro <turndn.up@gmail.com>
2017-03-11 02:33:52 +0900

    Merge upstearm/master into master

--- home/.bashrc
+++ home/.bashrc
@@ -8,8 +8,6 @@ case $- in
       *) return;;
 esac
 
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
 HISTCONTROL=ignoreboth
 
 # append to the history file, don't overwrite it
--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ DOTFILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [[ -z "$DOTFILE_DIR" ]] && DOTFILE_DIR=~/.config/dotfiles
 
 main() {
-  local install_deps=""
+  local install_plugins=""
   for n in "$@"; do
     case "$n" in
-      --install-deps)
-        install_deps=yes
+      --install-plugins)
+        install_plugins=yes
         ;;
       *)
         ;;
@@ -17,17 +17,17 @@ main() {
 
   cd "$DOTFILE_DIR"
 
-  echo "$(tput bold)== Cloning submodules ==$(tput sgr0)"
-  git submodule update --init
+  echo "$(tput bold)== Updating submodules ==$(tput sgr0)"
+  git submodule update --init --remote
 
   echo "$(tput bold)== Installing configuration ==$(tput sgr0)"
   setup::shell
   setup::vim
   setup::misc
 
-  if [[ -n "$install_deps" ]]; then
-    echo "$(tput bold)== Installing dependencies ==$(tput sgr0)"
-    setup::install_deps
+  if [[ -n "$install_plugins" ]]; then
+    echo "$(tput bold)== Installing plugins ==$(tput sgr0)"
+    setup::install_plugins
   fi
 }
 
@@ -100,9 +100,9 @@ setup::gpg() {
     mkdir ~/.gnupg
     chmod 700 ~/.gnupg
   fi
-  chmod 700 $DOTFILE_DIR/home/.gnupg
-  chmod 600 $DOTFILE_DIR/home/.gnupg/gpg-agent.conf
-  chmod 600 $DOTFILE_DIR/home/.gnupg/gpg.conf
+  chmod 700 "$DOTFILE_DIR/home/.gnupg"
+  chmod 600 "$DOTFILE_DIR/home/.gnupg/gpg-agent.conf"
+  chmod 600 "$DOTFILE_DIR/home/.gnupg/gpg.conf"
   install_symlink ".gnupg/gpg-agent.conf"
   install_symlink ".gnupg/gpg.conf"
 }
@@ -111,12 +111,13 @@ setup::misc() {
   install_symlink ".config/git/config"
   install_symlink ".config/git/ignore"
   install_symlink ".gdbinit"
+  install_symlink ".local/libexec/fzf/install"
   install_symlink ".local/opt/peda"
   install_symlink ".local/opt/pwndbg"
   install_symlink ".tmux.conf"
 }
 
-setup::install_deps() {
+setup::install_plugins() {
   sudo apt-get update
   sudo apt-get install -y \
     build-essential \
